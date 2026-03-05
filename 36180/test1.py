38bbe41a0e1ff443c3fd2429c373b4fedcb79030
Batuhan mert Elma <batuhanmertelma20@gmail.com>
2026-03-05 10:40:08 +0000

    last change

--- 36180/test1.py
+++ 36180/test1.py
@@ -7,3 +7,9 @@ if num > 5:
   print("bigger than num")
 else :
   print("smaller than num") 
+
+print("-----------------------------")
+
+animals =["aslan, kedi, yilan"]
+for i in animals:
+    print(i)
