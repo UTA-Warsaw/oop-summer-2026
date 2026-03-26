f060336ab188ae793bb7c12f0442aeaf93c2cf50
jessicahayalkaratokus <hayal.karatokus@gmail.com>
2026-03-26 10:31:52 +0000

    test4

--- student/36205/lecture_4_test.py
+++ student/36205/lecture_4_test.py
@@ -1,1 +1,12 @@
+# jessica_first_project_with_iskender
 
+# long variable name
+jessica_favorite_student_number = 2
+
+# constant
+MAX_POINTS = 70
+
+# class
+class Student:
+    def _init_ (self, name):
+        self.name = jessica
