96b441cb5d0c85af653ffca954e7c45c7313104f
Selim Yildiz <97531756+667revo@users.noreply.github.com>
2026-04-16 10:46:43 +0200

    Create student.py

---
+++ students/34551/student.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, grade, index_number):
+        self.name = name
+        self.grade = grade
+        self.index_number = index_number
+
+s1 = Student("Selim", "F", 34551)
+
+print(s1.index_number)
