43f9233bc70c8e200ceea509e2b2f9e524bd45dc
Selim Yildiz <97531756+667revo@users.noreply.github.com>
2026-04-16 10:57:49 +0200

    Add Rectangle class with area and perimeter methods

---
+++ students/34551/rectangle.py
@@ -0,0 +1,15 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+    
+r1 = Rectangle(5, 3)
+
+print("Area of Rectangle is =", r1.area())
+print("Perimeter of Rectangle is =", r1.perimeter())
